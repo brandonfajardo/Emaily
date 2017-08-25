96f7eeace8024e3caded78da3fda63bfa29fb808
brandonjfajardo <brandonjfajardo@hotmail.com>
2017-08-24 22:00:12 -0700

    change again

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send({ hi: 'there'})
+    res.send({ bye: 'now'})
 })
 
 const PORT = process.env.PORT || 5000 // process.env.PORT is for production... 5000 is for dev
