f53694e6e7051550f15722e8d545d8ff4ca16049
brandonjfajardo <brandonjfajardo@hotmail.com>
2017-08-31 00:32:32 -0700

    payment route

--- index.js
+++ index.js
@@ -3,16 +3,20 @@ const mongoose = require('mongoose')
 const keys = require('./config/keys')
 const cookieSession = require('cookie-session')
 const passport = require('passport')
+const bodyParser = require('body-parser')
+
 require('./services/passport')
 
 mongoose.connect(keys.mongoURI)
 
 const app = express()
+app.use(bodyParser.json())
 app.use(cookieSession({ maxAge: 30 * 24 * 60 * 60 * 1000, keys: [keys.cookieKey] }))
 app.use(passport.initialize())
 app.use(passport.session())
 
 require('./routes/authRoutes')(app)
+require('./routes/billingRoutes')(app)
 
 const PORT = process.env.PORT || 5000 // process.env.PORT is for production... 5000 is for dev
 app.listen(PORT, () => {
